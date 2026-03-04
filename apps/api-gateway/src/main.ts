6cfb13247b821534055c67be7dd2c1b241a53be6
sajithfdo <sajith@teachmeit.lk>
2026-03-04 15:50:32 +0530

    added tests
    added tests

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
@@ -13,5 +13,10 @@ async function bootstrap() {
   app.use(cookieParser());
 
   await app.listen(process.env.GATEWAY_HTTP_PORT || 3000, '0.0.0.0');
+
+  return app;
 }
-bootstrap();
+
+if (require.main === module) {
+  bootstrap();
+}
